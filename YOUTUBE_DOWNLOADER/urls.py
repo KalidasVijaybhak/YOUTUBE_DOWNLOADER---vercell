24f1d960529253dd686858bf3deacadc65b98593
Kalidas <calidazvb@gmail.com>
2024-02-03 19:32:54 +0530

    final touch

--- YOUTUBE_DOWNLOADER/urls.py
+++ YOUTUBE_DOWNLOADER/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from YOUTUBE_DOWNLOADER import settings
-
+from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('downloader.urls')),
